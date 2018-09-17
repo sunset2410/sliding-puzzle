eac17c3f6f4853ec4123558dbe83b558c119fa0e
sunset <chieubkhn@gmail.com>
2018-09-17 23:07:57 +0700

    game state

---
+++ Boot.js
@@ -0,0 +1,19 @@
+var Game ={};
+
+
+Game.boot = function(game){
+    console.log("boot");
+
+}
+
+
+Game.boot.prototype ={
+    preload: function(){
+
+    },
+
+    create: function(){
+        
+    }
+
+}
